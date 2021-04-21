73f81fa49eafc7e5c8959ae908990bf0987e59b3
André Almeida <andrebot_almeida@hotmail.com>
2021-04-21 18:20:34 -0300

    Added progress bar

---
+++ src/client/components/progressbar/progressbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './progressbar.styl';
+
+const ProgressBar = () => {
+  return (
+    <div className="progress-bar">
+      <div className="progress"></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
