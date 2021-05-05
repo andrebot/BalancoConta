2cdb07a589afa2adb7f65f8d164c3eee6cee7098
André Almeida <andrebot_almeida@hotmail.com>
2021-05-05 18:14:43 -0300

    First table's draft done

--- src/client/pages/accountPage/accountPage.jsx
+++ src/client/pages/accountPage/accountPage.jsx
@@ -5,9 +5,38 @@ import './accountPage.styl';
 export default () => {
   return (
     <div className="container recurrent-expanses">
-      <div></div>
-      <span>Recurrent Expanses</span>
-      <Button icon="/icons/plus.svg" isCircle={true}/>
+      <div className="container-title">
+        <div></div>
+        <span>Recurrent Expanses</span>
+        <Button icon="/icons/plus.svg" isCircle={true}/>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Due Day</th>
+            <th>Type</th>
+            <th>Value</th>
+            <th>Paid</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Condomínio</td>
+            <td>15</td>
+            <td>Casa</td>
+            <td>R$ 950,00</td>
+            <td><input type="checkbox" /></td>
+          </tr>
+          <tr>
+            <td>Contador</td>
+            <td>10</td>
+            <td>Empresa</td>
+            <td>R$ 576,00</td>
+            <td><input type="checkbox" /></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
