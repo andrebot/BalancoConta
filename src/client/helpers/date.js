eea83fa5eea5184dacc5ba8fb04451c63c0b8539
André Almeida <andrebot_almeida@hotmail.com>
2021-05-17 16:03:44 -0300

    Adding some documentation

--- src/client/helpers/date.js
+++ src/client/helpers/date.js
@@ -8,10 +8,53 @@ for (let year = 1900; year < 2100; year++) {
   years.push(year);
 }
 
+/**
+ * Returns the Month's abreviation. The month count starts at 0, where 0
+ * is January.
+ *
+ * @param {number} month Number representing the month
+ * @returns {string} The month's name abreviation
+ */
 export const getMonthShort = month => monthsShort[month];
+/**
+ * Returns the Month's name. The month count starts at 0, where 0
+ * is January.
+ *
+ * @param {number} month Number representing the month
+ * @returns {string} the month's name
+ */
 export const getMonth = month => months[month];
+/**
+ * Returns the weekday's name. The weekday count starts at 0, where 0
+ * is Sunday.
+ *
+ * @param {number} day Number representing the weekday
+ * @returns {string} the weekday's name
+ */
 export const getWeekDay = day => weekDays[day];
+/**
+ * Returns the weekday's abreviation. The weekday count starts at 0, where 0
+ * is Sunday.
+ *
+ * @param {number} day Number representing the weekday
+ * @returns {string} the weekday's abreviation
+ */
 export const getWeekDayShort = day => weekDaysShort[day];
+/**
+ * Return a list of all Month's names
+ *
+ * @returns {string[]} list with all month's name
+ */
 export const getMonths = () => months;
+/**
+ * Return a list of all weekday's abreviation
+ *
+ * @returns {string[]}
+ */
 export const getWeekDaysShort = () => weekDaysShort;
+/**
+ * Rerturns a list of the 100 years. Starting 50 years from now
+ *
+ * @returns {number[]} List of a hundred years
+ */
 export const getYears = () => years;
